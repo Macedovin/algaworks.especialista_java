effd380e401f00ad3415a76a754fbe160e59f1ed
Vinicius Macedo <macedo.vp@gmail.com>
2024-02-09 10:55:03 -0300

    Refactor equality, unary negation and 'not equal to' operators
    Revisiting the concepts and the class file notes turning it better to read and understand

--- 03.Estrturas_controle-operadores/3.01.Operadores-Igualdade-Unario_negacao/OperadoresIgualdadeNegacao.java
+++ 03.Estrturas_controle-operadores/3.01.Operadores-Igualdade-Unario_negacao/OperadoresIgualdadeNegacao.java
@@ -4,53 +4,57 @@ public class OperadoresIgualdadeNegacao {
 		
 		// - FORMAS LITERAIS
 		// boolean numerosIguais = 10 == 10;
-		// System.out.printf("Números iguais: %b%n", numerosIguais);
+		// System.out.printf("Números iguais: %b%n", numerosIguais);                 // TRUE
 		
 		// - OPERAÇÕES ARITMÉTICAS
 		// boolean numerosIguais = (4 + 6) == (5 * 2);
-		// System.out.printf("Operações de resulados iguais: %b%n", numerosIguais);
+		// System.out.printf("Operações de resulados iguais: %b%n", numerosIguais);   // TRUE
 	
 		// - VALORES DE VARIÁVEIS
 		int numero1 = 10;
 		int numero2 = 10;
 		int numero3 = 11;
 		
-		// boolean numerosIguais = numero1 == 10;
-		// System.out.printf("Variável e número iguais: %b%n", numerosIguais);
-		// boolean numerosIguais = numero1 == numero2;
-		// System.out.printf("Variáveis iguais: %b%n", numerosIguais);
 		
-		// System.out.printf("Variáveis iguais: %b%n", numero1 == numero3);
+		// - FORMAS LITERAIS e VARIÁVEIS
+		// boolean numerosIguais = numero1 == 10;
+		// System.out.printf("Variável e número iguais: %b%n", numerosIguais);        // TRUE
+		
+		// - VARIÁVEIS
+		// boolean numerosIguais = numero1 == numero2;
+		// System.out.printf("Variáveis iguais: %b%n", numerosIguais);                // TRUE
+		// System.out.printf("Variáveis iguais: %b%n", numero1 == numero3);           // FALSE
 		
 		// - OPERADOR UNÁRIO DE NEGAÇÃO
 		// boolean numerosIguais = numero1 == numero3;
-		// System.out.printf("Variáveis iguais: %b%n", numerosIguais);
+		// System.out.printf("Variáveis iguais: %b%n", numerosIguais);                // FALSE
 		
+		// Negando VARIÁVEIS
 		// boolean numerosDiferentes = !numerosIguais;
-		// System.out.printf("Variáveis diferentes: %b%n", numerosDiferentes);	
+		// System.out.printf("Variáveis diferentes: %b%n", numerosDiferentes);	      // TRUE
 		
-		// Negando a EXPRESSÃO BOOLEANA
+		// Negando EXPRESSÃO BOOLEANA -> CUIDADO com a legibilidade do código
 		// boolean numerosDiferentes = !(numero1 == numero2);
-		// System.out.printf("Variáveis diferentes: %b%n", numerosDiferentes);
+		// System.out.printf("Variáveis diferentes: %b%n", numerosDiferentes);        // FALSE
 		
 		// - OPERADOR DE DIFERENÇA
 		// boolean numerosDiferentesReal = numero1 != numero2;
-		// System.out.printf("Variáveis realmente diferentes: %b%n", numerosDiferentesReal);
+		// System.out.printf("Variáveis realmente diferentes: %b%n", numerosDiferentesReal);  // FALSE
 		
 		// - COMPARANDO STRINGS
 		String nome1 = "Thiago";
 		String nome2 = "Vinicius";
 		String nome3 = "Vinicius";
 		
-		// boolean nomesIguais = nome1 == nome2;
-		// System.out.printf("Nomes iguais: %b%n", nomesIguais);
-		// System.out.printf("Nomes iguais: %b%n", nome2 == nome3);
+		// boolean nomesIguais = nome1 == nome2;                                     //             
+		// System.out.printf("Nomes iguais: %b%n", nomesIguais);                     // FALSE  
+		// System.out.printf("Nomes iguais: %b%n", nome2 == nome3);                  // TRUE 
 		
 		// - LIMITAÇÕES -> IGUALDADE 
 		
 		// * Compara a localização dos objetos na memória *
 		
-		// Números Inteiros até 127 ou 1 byte
+		// Números Inteiros até 127 ou 1 byte ou Long até 127L -> Geralmente ficam em CACHE na memória
 		Integer numeroI1 = 128;
 		Integer numeroI2 = 128;
 		
@@ -61,13 +65,13 @@ public class OperadoresIgualdadeNegacao {
 		boolean numerosInteirosIguais = numeroI1 == numeroI2;
 		boolean numerosIntIguais = numeroInt1 == numeroInt2;
 			
-		System.out.printf("Números Inteiros iguais: %b%n", numerosInteirosIguais);
-		System.out.printf("Números int iguais: %b%n", numerosIntIguais);
+		System.out.printf("Números Inteiros iguais: %b%n", numerosInteirosIguais);         // FALSE
+		System.out.printf("Números int iguais: %b%n", numerosIntIguais);                   // TRUE
 		
-		// String são calsses e podem gerar objetos com diferentes espaços na memória
+		// String são classes e podem gerar objetos com diferentes espaços na memória
 	    String s1 = "Sachin";  
 	    String s2 = new String("Sachin");  
-	    System.out.printf("Igualdade de objetos (==): %b%n", s1 == s2);
-	    System.out.printf("Igualdade de objetos(equals()): %b%n", s1.equals(s2));
+	    System.out.printf("Igualdade de objetos (==): %b%n", s1 == s2);                    // FALSE - Diferentes OBJETOS
+	    System.out.printf("Igualdade de objetos(equals()): %b%n", s1.equals(s2));          // TRUE - Mesmo conteúdo
 	}
 }	
