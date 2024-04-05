aaa31f7c0ac2fd65064033aa7e4f0a15f2ef81c9
Vinicius Macedo <macedo.vp@gmail.com>
2024-04-05 15:32:06 -0300

    Access object attributes

--- 05.Mergulhando-Orientacao_Objetos/aula-oo/src/Principal.java
+++ 05.Mergulhando-Orientacao_Objetos/aula-oo/src/Principal.java
@@ -4,6 +4,29 @@ public class Principal {
 
         Carro meuCarro = new Carro();
 
+//        Definindo ATRIBUTOS do OBEJTO meuCarro
+        meuCarro.anoFabricacao = 2023;
+        meuCarro.cor = "preto";
+        meuCarro.modelo = "HR-V";
+        meuCarro.fabricante = "Honda";
+
         Carro seuCarro = new Carro();
+
+        seuCarro.anoFabricacao = 2021;
+        seuCarro.cor = "Branco";
+        seuCarro.modelo = "X6";
+        seuCarro.fabricante = "BMW";
+
+        System.out.println("Meu carro");
+        System.out.println("------------------------");
+        System.out.printf("Modelo: %s%n", meuCarro.modelo);
+        System.out.printf("Ano: %d%n", meuCarro.anoFabricacao);
+
+        System.out.println(); //-> Linha em branco/ QUEBRA DE LINHA
+
+        System.out.println("Seu carro");
+        System.out.println("------------------------");
+        System.out.printf("Modelo: %s%n", seuCarro.modelo);
+        System.out.printf("Ano: %d%n", seuCarro.anoFabricacao);
     }
 }
